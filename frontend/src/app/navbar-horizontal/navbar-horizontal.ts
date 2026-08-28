25eb6c628ced11b702e997d5cd4ef97000148c07
Ignacio Moreira <ignaciomoreira2121@gmail.com>
2026-08-27 23:52:23 -0300

    feat(routing): convertir enlaces a router link

--- frontend/src/app/navbar-horizontal/navbar-horizontal.ts
+++ frontend/src/app/navbar-horizontal/navbar-horizontal.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar-horizontal',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar-horizontal.html',
   styleUrl: './navbar-horizontal.css',
 })
