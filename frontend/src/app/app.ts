7c89a1a7493449cbeefac49a28c54fb30db079e3
Ignacio Moreira <ignaciomoreira2121@gmail.com>
2026-08-27 23:08:30 -0300

    feat(footer): creacion de footer

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { Footer } from './footer/footer';
 import { NavbarHorizontal } from './navbar-horizontal/navbar-horizontal';
 import { QuienesSomos } from './quienes-somos/quienes-somos';
 
 @Component({
     selector: 'app-root',
-    imports: [NavbarHorizontal, QuienesSomos],
+    imports: [Footer, NavbarHorizontal, QuienesSomos],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
