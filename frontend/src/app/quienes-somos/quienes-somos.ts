04f9a71993900d54589123154a9a09822d930842
rodrichc <rodrichc1@gmail.com>
2026-09-20 17:07:17 -0300

    fix: corregir apartado integrantes del equipo, agregar changeDetector y cambio en urls de foto

--- frontend/src/app/quienes-somos/quienes-somos.ts
+++ frontend/src/app/quienes-somos/quienes-somos.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { Integrante } from '../core/models/integrante.model';
 import { IntegrantesService } from '../core/services/integrantes.service';
 
@@ -12,12 +12,16 @@ export class QuienesSomos implements OnInit {
 
   integrantes: Integrante[] = [];
 
-  constructor(private integrantesService: IntegrantesService) {}
+  constructor(
+    private integrantesService: IntegrantesService,
+    private cdr: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     this.integrantesService.getIntegrantes().subscribe({
       next: (datos) => {
         this.integrantes = datos;
+        this.cdr.detectChanges()
       },
       error: (error) => {
         console.error('Error al cargar los integrantes:', error);
